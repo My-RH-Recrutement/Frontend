8720893e449d1b6d62821ee4232a199b43bc462f
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-01-30 02:07:08 +0100

    add hydration meta reducer to persist state when page refresh

--- src/app/ngrx/auth/auth.state.module.ts
+++ src/app/ngrx/auth/auth.state.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
-import { StoreFeatureModule, StoreModule } from "@ngrx/store";
+import { StoreModule } from "@ngrx/store";
 import { authFeatureKey, authReducer } from "./auth.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import * as registerEffects from "./auth.effects";
+import { metaReducers } from ".";
 
 @NgModule({
     imports: [
-        StoreModule.forFeature(authFeatureKey, authReducer),
+        StoreModule.forFeature(authFeatureKey, authReducer, {
+            metaReducers: metaReducers, 
+        }),
         EffectsModule.forFeature(registerEffects)
     ]
 })
